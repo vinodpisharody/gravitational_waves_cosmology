6897409b1d855458c5f74d1f6e796415744831ea
Vinod Pisharody <vinod.pisharody22@gmail.com>
2023-09-14 18:27:21 +0530

    made some QOL changes in computing background
    1.Added a attribute R0 to accomodate for mass dependent merger rate for PBH
    2.Changed the way omega is to compute background so  that all calculations are done in one go saving a lot of time.
    3.Absolute error is now set to 0 and the error of integration is now controlled by relative error (epsrel) .Default being 0.01 corresponding to 1 % Error
    4.Added detector factor new file that considers masses all the way to 1 M0

--- sgwb.py
+++ sgwb.py
@@ -1,16 +1,34 @@
 from scipy.integrate import quad
+from scipy.integrate import tplquad
 import ray
 import numpy as np
 from scipy import interpolate
 import warnings
 import functools
 import matplotlib.pyplot as plt
+import binary_snr as bns
+import pandas as pd
 warnings.filterwarnings("ignore",category=RuntimeWarning)
 G=6.67*10**(-11)
 M=2*10**30
 c=299792458
 year=365*24*3600
 mpc=3.08568*10**22
+
+data_stellar=pd.read_csv('stellar_pop3_opt.csv')
+
+def detection_factor(m1,m2,z,detector=None):
+    if m1<m2:
+        raise ValueError('Primary mass cannot be less than secondary mass')
+    if detector not in ['LISA', 'CE1','CE2','ET']:
+        raise ValueError('Unknown Detector')
+    columns = ['m1', 'm2', 'LISA', 'CE1','CE2','ET']
+    df = pd.DataFrame(data_stellar, columns=columns)
+    matching_rows = df[(df['m1'] == round(m1)) & (df['m2'] == round(m2))]
+    if z>matching_rows.iloc[0][detector]:
+         return 1
+    else:
+         return 0
 def waveform_factor(f,z,m1,m2,flag=None,detector=None):
   fm=[2.9740*10**(-1),4.4810*10**(-2),9.5560*10**(-2)]
   fri=[5.9411*10**(-1),8.9794*10**(-2),19.111*10**(-2)]
@@ -45,6 +63,7 @@ def waveform_factor(f,z,m1,m2,flag=None,detector=None):
       else:
         return 0
 
+
 @ray.remote
 class background():
     """
@@ -60,7 +79,7 @@ class background():
         detector(string):LISA or DECIGO to only consider the inpiral case .Default None to include the merger and ringdown phase
         verbose(bool):To show/hide warnings
     """
-    def __init__(self,merger,primary_mass,secondary_mass,H0=70,om_m=0.3,om_l=0.7,frequency_band=None,detector=None,verbose=True):
+    def __init__(self,merger,primary_mass,secondary_mass,H0=70,om_m=0.3,om_l=0.7,R0=lambda m1,m2:1,frequency_band=None,detector=None,verbose=True):
         self.H0=H0
         self.om_m=om_m
         self.om_l=om_l
@@ -81,10 +100,13 @@ class background():
         else:
             raise ValueError("Frequency not specified")
         self.detector=detector
-        if detector==None and verbose==True:
+        if detector==None:
+            raise ValueError('Detector not specified')
+        elif detector=='LISA' and verbose==True:
             warnings.warn('You have not specified the detector.So all of the inspiral-merger-ringdown will be considered in background',category=UserWarning)
-
-    def get_background(self):
+        self.verbose=verbose
+        self.R0=R0
+    def get_background(self,tol_m=0.01,tol_z=0.01):
         def memoize(maxsize=None):
             def decorator(func):
                 cache = {}
@@ -108,25 +130,25 @@ class background():
         prefactor=(M**(5/3)*((G*np.pi)**(2/3)))/(3*rho*(c**2)*(self.H0*1000*((mpc)**-1))*((mpc*1000)**3)*year)
         probability_function_primary  =  lambda m,z : self.primary_mass[0](m,z);m1_min=self.primary_mass[1];m1_max=self.primary_mass[2]
         probability_function_secondary=  lambda m,z : self.secondary_mass[0](m,z);m2_min=self.secondary_mass[1];m2_max=self.secondary_mass[2]
+        m=np.linspace(5,100,endpoint=False)
         z_lower,z_upper=self.merger[1],self.merger[2];rem_rate=lambda z:self.merger[0](z)
-        print(z_lower)
         global omega
         g_sw=np.empty(0)
         probability_function=lambda m1,m2,z:probability_function_primary(m1,z)*probability_function_secondary(m2,z)
         @ray.remote
         def omega(f):
-                integrand = lambda z, m1, m2: waveform_factor(f,z,m1,m2,detector=self.detector)*((m1*m2)/(m1+m2)**(1/3)) \
-                *probability_function(m1, m2,z)*(1/((((self.om_m*((1+z)**3))+self.om_l)**(0.5))*(1+z)))*(rem_rate(z)
+                integrand = lambda z, m1, m2:(10**5)*prefactor*f*waveform_factor(f,z,m1,m2,detector=self.detector)*(detection_factor(m1=m1, m2=m2, z=z, detector=self.detector))*((m1*m2)/(m1+m2)**(1/3)) \
+                *probability_function(m1, m2,z)*(1/((((self.om_m*((1+z)**3))+self.om_l)**(0.5))*(1+z)))*(self.R0(m1,m2)*rem_rate(z)
                                 /(normalizer(lambda m:probability_function_secondary(m,z),m2_min,m2_max)*normalizer(lambda m:probability_function_primary(m,z),m2,m1_max)))
                 def m1_integral(m2, z):
-                    a=quad(lambda m1: integrand(z, m1, m2),m2, m1_max, epsrel=10, epsabs=10)[0]
-                    return a
+                    a=quad(lambda m1: integrand(z, m1, m2),m2, m1_max, epsrel=tol_m, epsabs=0,limit=100)
+                    return a[0]/10**5
                 def m2_integral(z):
-                    return quad(lambda m2: m1_integral(m2, z), m2_min, m2_max, epsrel=10, epsabs=10)[0]
-                result, _ = quad(lambda z: m2_integral(z), z_lower, z_upper, epsrel=1, epsabs=1)
-                return prefactor * f * result
+                    return quad(lambda m2: m1_integral(m2, z), m2_min, m2_max, epsrel=tol_m, epsabs=0,limit=100)[0]
+                result = quad(lambda z: m2_integral(z), z_lower, z_upper, epsrel=tol_z, epsabs=0,limit=100)
+                return  result[0]
         if len(f)<10:
-            return ray.get(omega.remote(f))
+            return np.array(ray.get([omega.remote(i) for i in f]))
         lower_band_cutoff=waveform_factor(0,0,m1_max, m1_max,flag='inspiral')/(1+z_upper)
         upper_band_cutoff=waveform_factor(0,0, m1_min, m1_min,flag='cutoff')
         condition_upper =( f>=upper_band_cutoff)
@@ -136,15 +158,34 @@ class background():
         condition_lower = (f<=lower_band_cutoff)
         mask_lower = np.where(condition_lower)
         lower_f = f[mask_lower]
-        g_lower=lower_f**(2/3)*(ray.get(omega.remote(lower_band_cutoff))/(lower_band_cutoff)**(2/3))
         condition_middle = (f>lower_band_cutoff) & (f<upper_band_cutoff)
         mask_middle= np.where(condition_middle)
         middle_f_ = f[mask_middle]
         if len(middle_f_)==0:
+            g_lower=lower_f**(2/3)*(ray.get(omega.remote(lower_band_cutoff))/(lower_band_cutoff)**(2/3))
             return np.concatenate((g_lower,g_upper))
-        middle_f=np.logspace(lower_band_cutoff,upper_band_cutoff,100)
-        print(len(middle_f))
-        g_sw=np.array(ray.get([omega.remote(i) for i in middle_f_]))
-        #g_sw=interpolate.interp1d(middle_f,g_sw,kind='linear',fill_value='extrapolate')(middle_f_)
-        print("done")
+        else:
+            if self.verbose==True:
+                print("Big computation",flush=True)
+            else:
+                pass
+        if len(middle_f_)>=100:
+            if self.verbose==True:
+                print('Sampling 100 linearly spaced frequency bins between {}Hz and {}Hz'.format(min(middle_f_),max(middle_f_)),flush=True)
+            middle_f=np.linspace(min(middle_f_),max(middle_f_),100)
+            g_sw=[omega.remote(i) for i in middle_f]
+            g_sw.append(omega.remote(lower_band_cutoff))
+            ans=np.array(ray.get(g_sw))
+            g_sw=ans[:-1]
+            g_lower=lower_f**(2/3)*(ans[-1]/(lower_band_cutoff)**(2/3))
+            temp=interpolate.interp1d(x=middle_f, y=g_sw,fill_value='extrapolate',kind='cubic')(middle_f_)
+            g_sw=temp
+        else:
+            g_sw=[omega.remote(i) for i in middle_f_]
+            g_sw.append(omega.remote(lower_band_cutoff))
+            ans=np.array(ray.get(g_sw))
+            g_sw=ans[:-1]
+            g_lower=lower_f**(2/3)*(ans[-1]/(lower_band_cutoff)**(2/3))
+        if self.verbose==True:
+            print("Completed")
         return np.concatenate((g_lower,g_sw,g_upper))
